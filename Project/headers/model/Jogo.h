855d5a2317703d1b007b536a4e65e8a3865f41f3
Pedro <1251355@isep.ipp.pt>
2026-05-14 18:52:24 +0100

    Acrescentar modelo quadricula

---
+++ Project/headers/model/Jogo.h
@@ -0,0 +1,8 @@
+//
+// Created by pedro on 14/05/2026.
+//
+
+#ifndef PROJETOFSOFT_JOGO_H
+#define PROJETOFSOFT_JOGO_H
+
+#endif //PROJETOFSOFT_JOGO_H
---
+++ Project/headers/model/Tabuleiro.h
@@ -0,0 +1,8 @@
+//
+// Created by pedro on 14/05/2026.
+//
+
+#ifndef PROJETOFSOFT_TABULEIRO_H
+#define PROJETOFSOFT_TABULEIRO_H
+
+#endif //PROJETOFSOFT_TABULEIRO_H
