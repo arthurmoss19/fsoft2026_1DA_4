8465e755f065b87b91674050871828aa967addd1
Tiago <1251486@isep.ipp.pt>
2026-05-16 19:34:57 +0100

    Traduzido para inglês

---
+++ Project/headers/dto/BoatDTO.h
@@ -0,0 +1,16 @@
+//
+// Created by tiago on 16/05/2026.
+//
+
+#ifndef FSOFT_BARCODTO_H
+#define FSOFT_BARCODTO_H
+
+struct BoatDTO {
+    std::string type;
+    int size;
+    bool destroyed;
+    int line;
+    int column;
+    bool direction;
+
+#endif //FSOFT_BARCODTO_H
