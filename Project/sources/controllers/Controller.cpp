61c86aa023a6a1856c5014e4cf2255c1a8b2b4dd
arthur <1250771@isep.ipp.pt>
2026-05-16 18:51:26 +0100

    Atualização dos controllers

--- Project/sources/controllers/Controller.cpp
+++ Project/sources/controllers/Controller.cpp
@@ -2,9 +2,7 @@
 // Created by arthu on 16/05/2026.
 //
 
-#include <iostream>
 #include <string>
-#include <list>
 #include "Controller.h"
 #include "Utils.h"
 
@@ -15,16 +13,52 @@ void Controller::run() {
     do {
         op = this -> view.menuMain();
         switch(op) {
-            case 1: runNewGame;
+            case 1: runNewGame();
                 break;
-            case 2: runRanking;
+            case 2: runRanking();
                 break;
-            case 3: runStatistics;
+            case 3: runStatistics();
                 break;
-            case 4: runHelpAndRules;
+            case 4: runHelpAndRules();
                 break;
             default:
                 break;
         }
-    } while(op != 0);
+    } while (op != 0);
+}
+
+void Controller::runNewGame() {
+    int op = -1;
+    do {
+        op = this -> view.menuNewGame();
+        switch(op) {
+
+        }
+    } while (op != 0);
+}
+
+void Controller::runRanking() {
+    int op = -1;
+    do {
+        op = this -> view.menuRanking();
+        switch(op) {
+
+        }
+    } while (op != 0);
+}
+
+void Controller::runStatistics() {
+    int op = -1;
+    do {
+        op = this -> view.menuStatiscs();
+        switch(op) {
+
+        }
+    } while (op != 0);
+}
+
+void Controller::runHelpAndRules() {
+    this -> view.printHelpAndRules();
+
+    Utils::getString("Pressione ENTER para voltar ao menu");
 }
