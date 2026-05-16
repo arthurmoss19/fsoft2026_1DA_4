6a03d3745f8ac20ebbf1c854818873e39017eaa1
Tiago <1251486@isep.ipp.pt>
2026-05-16 18:22:03 +0100

    Criado BarcoDTO.h

---
+++ Project/headers/dto/BarcoDTO.h
@@ -0,0 +1,13 @@
+//
+// Created by tiago on 16/05/2026.
+//
+
+#ifndef FSOFT_BARCODTO_H
+#define FSOFT_BARCODTO_H
+
+struct BarcoDTO {
+    std::string tipo;
+    int tamanho;
+    bool afundado;
+
+#endif //FSOFT_BARCODTO_H
