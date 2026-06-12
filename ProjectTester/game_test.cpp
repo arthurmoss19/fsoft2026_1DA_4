438e62d5762a01ee6a064a33980a4e2091180837
Pedro <1251355@isep.ipp.pt>
2026-06-12 19:57:48 +0100

    adicao teste isVsComputer

--- ProjectTester/game_test.cpp
+++ ProjectTester/game_test.cpp
@@ -16,3 +16,21 @@ TEST(GameConstructorTest, ValidGame) {
     EXPECT_FALSE(g.isGameOver());
     EXPECT_EQ(g.getCurrentTurn(), 1);
 }
+
+
+TEST(GameIsVsComputerTest, JvsJ) {
+    Player p1("pedro");
+    Player p2("arthur");
+
+    Game g(&p1, &p2, false, 0);
+    EXPECT_FALSE(g.isVsComputer());
+}
+
+
+TEST(GameIsVsComputerTest, JvsComputer) {
+    Player p1("pedro");
+
+    Game g(&p1, nullptr, true, 1);
+
+    EXPECT_TRUE(g.isVsComputer());
+}
