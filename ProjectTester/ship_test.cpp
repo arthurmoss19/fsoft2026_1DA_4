901907e46decf294345fc9899e48f98192d2901b
Pedro <1251355@isep.ipp.pt>
2026-06-12 17:10:34 +0100

    Adicao teste para o contrutor da classe ship

--- ProjectTester/ship_test.cpp
+++ ProjectTester/ship_test.cpp
@@ -1,3 +1,17 @@
 //
 // Created by pedro on 12/06/2026.
 //
+
+#include "gtest/gtest.h"
+#include "Ship.h"
+
+TEST(ShipConstructorTest, ValidShip) {
+    bool flag = false;
+
+    try {
+        Ship s("Fragata", 2, '#');
+    } catch (...) {
+        flag = true;
+    }
+    EXPECT_FALSE(flag);
+}
