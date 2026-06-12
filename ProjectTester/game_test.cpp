e1fd98dcd665f357686abbd0e709a9205c150d75
Pedro <1251355@isep.ipp.pt>
2026-06-12 19:47:57 +0100

    adicao teste para o contrutor da classe game

--- ProjectTester/game_test.cpp
+++ ProjectTester/game_test.cpp
@@ -1,3 +1,18 @@
 //
 // Created by pedro on 12/06/2026.
 //
+
+#include "gtest/gtest.h"
+#include "Game.h"
+#include "Player.h"
+#include "Ship.h"
+
+TEST(GameConstructorTest, ValidGame) {
+    Player p1("pedro");
+    Player p2("arthur");
+
+    Game g(&p1, &p2, false, 0);
+
+    EXPECT_FALSE(g.isGameOver());
+    EXPECT_EQ(g.getCurrentTurn(), 1);
+}
