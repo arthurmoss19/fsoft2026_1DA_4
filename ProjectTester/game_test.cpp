c1bbea8743b504ffdbfd3975beda1565ef79f82f
Pedro <1251355@isep.ipp.pt>
2026-06-12 20:33:29 +0100

    adicao teste fim de jogo para a classe game

--- ProjectTester/game_test.cpp
+++ ProjectTester/game_test.cpp
@@ -60,3 +60,20 @@ TEST(GameExecuteMoveTest, HitKeepsTurn) {
 
     EXPECT_EQ(g.getCurrentTurn(), 1);
 }
+
+
+
+TEST(GameIsGameOverTest, GameOverWhenAllShipsSunk) {
+    Player p1("pedro");
+    Player p2("arthur");
+
+    Game g(&p1, &p2, false, 0);
+    Ship s("Fragata", 2, '#');
+
+    g.getBoard2().placeShip(s, 0, 0, true);
+
+    g.executeMove(0, 0);
+    g.executeMove(0, 1);
+
+    EXPECT_TRUE(g.isGameOver());
+}
