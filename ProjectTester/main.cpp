7f90acbc8df341b0e18790fb9a8af3313d940ed1
Pedro <1251355@isep.ipp.pt>
2026-06-12 14:46:35 +0100

    Adicao dos testes da classe player

---
+++ ProjectTester/main.cpp
@@ -0,0 +1,11 @@
+//
+// Created by pedro on 12/06/2026.
+//
+
+#include "gtest/gtest.h"
+#include "player_test.cpp"
+
+int main(int argc, char **argv) {
+    ::testing::InitGoogleTest(&argc, argv);
+    return RUN_ALL_TESTS();
+}
