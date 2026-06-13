66d00dbfa5b6292157671a8aca9ec64ee5ee7105
Pedro <1251355@isep.ipp.pt>
2026-06-13 11:29:13 +0100

    Atualizacao main.cpp

--- ProjectTester/main.cpp
+++ ProjectTester/main.cpp
@@ -6,6 +6,7 @@
 #include "player_test.cpp"
 #include "ship_test.cpp"
 #include "game_test.cpp"
+#include "game_test.cpp"
 
 int main(int argc, char **argv) {
     ::testing::InitGoogleTest(&argc, argv);
