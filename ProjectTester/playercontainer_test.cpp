32a9aa639c2b4f215bdce2926f54bb41adcbefdb
Pedro <1251355@isep.ipp.pt>
2026-06-13 13:16:32 +0100

    Adicao teste PlayerContainer

--- ProjectTester/playercontainer_test.cpp
+++ ProjectTester/playercontainer_test.cpp
@@ -1,3 +1,17 @@
 //
 // Created by pedro on 13/06/2026.
 //
+
+#include "gtest/gtest.h"
+#include "PlayerContainer.h"
+#include "NoDataException.h"
+
+TEST(PlayerContainerAddTest, AddAndGet) {
+    PlayerContainer pc;
+
+    pc.add("pedro");
+    Player* p = pc.get("pedro");
+
+    EXPECT_NE(p, nullptr);
+    EXPECT_TRUE(*p == "pedro");
+}
